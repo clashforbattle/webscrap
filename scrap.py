867609fc8879d984e43ecf45ac6545f37df658c5
HacLin <muthuhari234@gmail.com>
2021-04-26 16:53:02 +0530

    Initial commit

--- scrap.py
+++ scrap.py
@@ -2,10 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
+import time
+from bs4 import BeautifulSoup
+import requests
+
+#constants
+URL = "https://www.tnebnet.org/atm/tariffMaster"
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.tnebnet.org/atm/tariffMaster")
+driver.get(URL)
+source = driver.page_source
 unit_consume = driver.find_element_by_xpath('//*[@id="calculator:unit"]')
 element = driver.find_element_by_xpath('//*[@id="calculator:tarifftype"]')
 calc_button = driver.find_element_by_xpath('/html/body/div[1]/table/tbody/tr[4]/td/fieldset/div/table[2]/tbody/tr/td[2]/button/span')
@@ -15,3 +22,8 @@ select_object.select_by_value("DOMESTIC")
 action.send_keys_to_element(unit_consume,500)
 action.click(on_element=calc_button)
 action.perform()
+
+# response = requests.get(URL)
+# print(response.text)
+soup = BeautifulSoup(source,'lxml')
+print(soup)

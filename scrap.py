42a91f1ba6dd99cac9796e68ba80ff1f1909f686
HacLin <muthuhari234@gmail.com>
2021-04-26 18:29:42 +0530

    Last commit

--- scrap.py
+++ scrap.py
@@ -5,15 +5,16 @@ from time import sleep
 import time
 from bs4 import BeautifulSoup
 import requests
+import csv
   
-  
+
 #constants
 URL = "https://www.tnebnet.org/atm/tariffMaster"
 
 driver = webdriver.Firefox()
 
 driver.get(URL)
-source = driver.page_source
+
 unit_consume = driver.find_element_by_xpath('//*[@id="calculator:unit"]')
 element = driver.find_element_by_xpath('//*[@id="calculator:tarifftype"]')
 calc_button = driver.find_element_by_xpath('/html/body/div[1]/table/tbody/tr[4]/td/fieldset/div/table[2]/tbody/tr/td[2]/button/span')
@@ -24,7 +25,17 @@ action.send_keys_to_element(unit_consume,500)
 action.click(on_element=calc_button)
 action.perform()
 
-# response = requests.get(URL)
-# print(response.text)
+sleep(3)
+
+
+source = driver.page_source
 soup = BeautifulSoup(source,'lxml')
-print(soup)
+
+panel_content = soup.find_all('span')
+rows = [panel_content[7].text,panel_content[-4].text]
+
+with open ('data.txt','r+') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(rows)
+
+
